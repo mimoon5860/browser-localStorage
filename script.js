e68c4216c3e6b5c00009fe3a7cd15a9c56603481
mimoon5860 <mahmodulislammoon@gmail.com>
2021-09-06 04:43:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const loadStorage = () => {
+    const cartStorage = getCart();
+    for (const item in cartStorage) {
+        displayItems(item, cartStorage[item])
+    }
+};
+
+const getValue = () => {
+    const itemName = document.getElementById('input-1');
+    const itemPrice = document.getElementById('input-2');
+    if (!itemPrice.value || !itemName.value) {
+        itemName.value = '';
+        itemPrice.value = '';
+        return;
+    }
+    else {
+        displayItems(itemName.value, itemPrice.value);
+        addProductToCart()
+    }
+
+    itemName.value = '';
+    itemPrice.value = '';
+};
+const displayItems = (name, price) => {
+    const cartUi = document.getElementById('display-cart');
+    const item = document.createElement('li');
+    item.innerHTML = `
+    ${name}: ${price}
+    `;
+    cartUi.appendChild(item);
+}
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart);
+    }
+    else {
+        cartObj = {};
+    }
+    return cartObj;
+};
+
+const addProductToCart = (name, price) => {
+    const cart = getCart();
+    cart[name] = price;
+    const cartStringfied = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringfied);
+}
+loadStorage()
